43a780aeef4838dccdc5c5938ae3216beda82de6
Hieu Nguyen <112587128+Leonerdo322@users.noreply.github.com>
2022-11-08 14:12:43 -0600

    Update Apocalypse.java

--- Apocalypse.java
+++ Apocalypse.java
@@ -1,1 +1,17 @@
-
+public class Apocalypse{
+ // obtain from person java class
+  Person person = new Person(Fight.NUM_PEOPLE,Fight.NUM_AMMO);
+  private int numAmmo = person.getNumbAmmo();
+  private int numPeople = person.getNumberPeople();
+ // declaring the instance variables
+  private int daySurvived;
+  private int numFood;
+  private final int totalDAys;
+  
+  
+  // designing contructor 
+  public Apocalypse(int numFood, int totalDay){
+  
+  }
+  
+}
