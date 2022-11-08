89643063d7e210691f2d938acb409510f5b86d3f
Hieu Nguyen <112587128+Leonerdo322@users.noreply.github.com>
2022-11-08 14:03:58 -0600

    Update ApocalypseSimulator.java

--- ApocalypseSimulator.java
+++ ApocalypseSimulator.java
@@ -1,5 +1,8 @@
 // creating a main class that runs the program, nothing will be in this main class 
 
 public class ApocalypseSimulator{
-
+  public static void main(String[] args){
+    Apocalypse apocalypse = new Apocalypse(150,10); // creating an instance variables for the numbers of days for the simulation
+    apocalypse.runSimulation(); // take from apocalypse class and method runSimulation
+  }
 }
